8544a289a7ae13c0d05271bc9babb3b0088802ab
MeghvShetty <meghvshetty@outlook.com>
2023-01-31 16:43:04 +0000

    update

---
+++ Python/two_fer.py
@@ -0,0 +1,17 @@
+def Give_name(name):
+    """Given name take a string in put as a parra 
+
+    Args:
+        :param name (string): name of the user 
+        :returns : string
+    """
+    return("One for "+name+", one for me.")
+    
+    # Teeting the Out 
+
+def main():
+    print(Give_name("maegh"))
+    
+
+if __name__ == '__main__':
+    main()
