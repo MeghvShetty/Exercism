98ddefdfe783b222bb294b537c01475f5a2ad206
MeghvShetty <meghvshetty@outlook.com>
2023-03-22 10:24:37 +0000

    AST update

--- Python/POC/AST.py
+++ Python/POC/AST.py
@@ -1,8 +1,8 @@
 import ast
 data_file = "/home/meghshetty/Documents/Projects/Exercism/Python/two_fer.py"
 data_terr = "/home/meghshetty/Documents/Projects/Exercism/Go_lang/astTester/terraform.tf"
-f = open(data_terr)
+f = open(data_file)
 data =f.read()
 code = "print('Hellowworld')"
-tree = ast.parse(code)
+tree = ast.parse(data)
 print (ast.dump(tree, indent=4))
